f19593548427abd24760590473d8d03e686d536a
Vikashtomar <vikash.tomar.india@gmail.com>
2024-09-23 17:06:29 +0530

    project h 1

--- color-sheme-Switcher/script.js
+++ color-sheme-Switcher/script.js
@@ -3,4 +3,27 @@ let body = document.querySelector("body");
 
 btns.forEach(function(btn){
     console.log(btn)
+
+    btn.addEventListener("click",(e)=>{
+        console.log(e.target)
+        if(e.target.id == "grey")
+        {
+            body.style.backgroundColor = "grey"
+        }
+        if(e.target.id == "white")
+            {
+                body.style.backgroundColor = "pink"
+            }
+            if(e.target.id == "blue")
+                {
+                    body.style.backgroundColor = e.target.id;
+                }
+                if(e.target.id == "yellow")
+                    {
+                        body.style.backgroundColor = "yellow"
+                    }
+                    
+                
+
+    })
 })
