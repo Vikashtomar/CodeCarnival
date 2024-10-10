27bb8056366fa60ceb5193f8de4172274cc49396
Vikashtomar <vikash.tomar.india@gmail.com>
2024-10-10 17:44:22 +0530

    have some file chnagement

--- guessthenumber/guessnum.js
+++ guessthenumber/guessnum.js
@@ -1,6 +1,87 @@
+ const randomNumber = parseInt(Math.random() *100 + 1)
 
-function randomNum(){
+ const submit = document.querySelector("#subt");
+ const userInput = document.querySelector("#guessField");
+ const guessSlot = document.querySelector(".guesses")
+ const remaining = document.querySelector(".lastResult")
+ const lowOrHi = document.querySelector(".lowOrhi")
+ const startOver = document.querySelector(".resultParas");
 
-    console.log(parseInt(Math.random() *10 +1))
+
+ const p = document.createElement("p")
+
+ let prevGuess = []
+ let numGuess = 1;
+
+ let playGame = true;
+
+ if(playGame){
+    submit.addEventListener("click",(e)=>{
+        e.preventDefault()
+        const guess = parseInt(userInput.value);
+        console.log(guess);
+        validateGuess(guess)
+    });
+ }
+
+
+ function validateGuess(guess){
+    if(isNaN(guess)){
+        alert("please enter a valid number")
+    }
+
+    else if (guess < 1){
+        alert("Plaese enter above 1")
+    }
+    else if(guess > 100){
+        alert("Please enter below 100")
+    }
+    else {
+        prevGuess.push(guess)
+        if(numGuess <= 10)
+        {
+         displayGuess(guess)   
+         displayMessage(`Game over Random number was ${randomNumber}`)
+         endGame()
+
+        }
+        else{
+            displayGuess(guess)
+            checkGuess(guess)
+        }
+    }
+ }
+
+
+function checkGuess(guess){
+    if(guess === randomNumber){
+        displayMessage("You guessed it correct!")
+        endGame()
+    }
+    else if(guess < randomNumber){
+        displayMessage(`Number is Too Low`)
+    }
+    else if(guess > randomNumber){
+        displayMessage(`Number is Too High`)
+    }
+    
+    if(numGuess >= 10){
+        displayMessage(`Game over number was ${randomNumber}`)
+    }
 }
+
+ function displayGuess(guess){
+    userInput.value = "";
+    guessSlot.innerHTML += `${guess}   `
+    numGuess++;
+    remaining.innerHTML  =`${10 - numGuess}`
+ }
+ 
+ function displayMessage(message){
+    lowOrHi.innerHTML = `<h2>${message} </h2>`
+ }
+
+ function endGame(){
+    //
+ }
 
