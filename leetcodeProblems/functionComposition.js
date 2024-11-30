d0ca78bef0c8b5f62d2c26b5810bc921ea89703c
Vikashtomar <vikash.tomar.india@gmail.com>
2024-11-30 22:40:32 +0530

    i correct it

--- leetcodeProblems/functionComposition.js
+++ leetcodeProblems/functionComposition.js
@@ -1,12 +1,20 @@
  
- let compose = function(functions){
-    return function(x){
-    let result  = x;
-    for(let i=functions.length-1; i >=0; i--){
-        result = functions[i][result]
-    }
-    return result;
-    }
- };
+//  let compose = function(functions){
+//     return function(x){
+//     let result  = x;
+//     for(let i=functions.length-1; i >=0; i--){
+//         result = functions[i][result]
+//     }
+//     return result;
+//     }
+//  };
 
- 
+let compose = function(functions) {
+    return function(x) {
+        let result = x;
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result);  
+        }
+        return result;
+    }
+};
