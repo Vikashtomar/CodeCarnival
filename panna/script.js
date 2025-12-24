c81f166d43bc6c08543c192a8e51d631fcaf61bb
Vikashtomar <vikash.tomar.india@gmail.com>
2025-12-24 09:34:36 +0530

    test commit

---
+++ panna/script.js
@@ -0,0 +1,8 @@
+  
+    // JavaScript
+    const button = document.getElementById('colorButton');
+    button.addEventListener('click', () => {
+      const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      document.body.style.backgroundColor = randomColor;
+    });
+  console.log("first");
