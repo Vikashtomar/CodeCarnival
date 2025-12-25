946d267bfa525413f627c4dab899a51f7581cca7
Vikashtomar <vikash.tomar.india@gmail.com>
2025-12-25 22:48:53 +0530

    a

--- videobg/script.js
+++ videobg/script.js
@@ -1,1 +1,8 @@
  console.log("Hello, World!");
+ let x = 10;
+    let y = 20;
+    console.log("Sum:", x + y);
+    function greet(name) {
+        return `Hello, ${name}!`;
+    }
+    console.log(greet("Vikash"));
