36657ba3769027eb318d36874dfd840795e0e9e7
Vikashtomar <vikash.tomar.india@gmail.com>
2026-01-07 12:53:39 +0530

    added functionality to pause and play btn

--- videobg/script.js
+++ videobg/script.js
@@ -40,4 +40,27 @@ console.log(factorial(3)); // Output: 6
 
 console.log("Script executed successfully.");
 
+// ab m video code likhunga
+const btn = document.querySelector(".btn");
+const video = document.querySelector(".background-video");
+
+const fa = document.querySelector(".fa")
+
+
+btn.addEventListener("click",()=>{
+    if(btn.classList.contains("pause")){
+btn.classList.remove("pause");
+   video.play();
+    fa.classList.add("fa-pause");
+    fa.classList.remove("fa-play");
+
+    } else {
+        btn.classList.add("pause");
+        video.pause();
+           
+    fa.classList.remove("fa-pause")
+    fa.classList.add("fa-play")
+    }
+ 
+})
 
